6d407f87aee7d69908054783b5f0093a6cde7837
m.abdallah.2023 <m.abdallah.2023@alumnos.urjc.es>
2026-05-20 18:26:44 +0200

    ej4 actualizado

--- src/testCases/ej4.for
+++ src/testCases/ej4.for
@@ -1,18 +1,98 @@
-PROGRAM pruebasDO;
+PROGRAM test_final;
 
-INTEGER :: a = 5;
-INTEGER :: b = 10;
-INTEGER :: c = 0;
+INTEGER, PARAMETER :: bin = b´1010´, oct = o´17´, hex = z´1F´;
 
-DO WHILE (a < b)
-    DO WHILE (c < 5)
-    c = c + 1;
+INTEGER :: a = 2, b = 3, c = 0;
+INTEGER :: resultado;
+
+REAL :: x = 2.5, y = 4.0, z;
+
+CHARACTER(30) :: texto = "hola ""mundo""";
+CHARACTER(20) :: texto2 = 'It''s ok';
+
+INTERFACE
+
+    FUNCTION Multiplicar(valor1, valor2)
+        REAL :: Multiplicar;
+        INTEGER, INTENT(IN) valor1;
+        REAL, INTENT(IN) valor2;
+    END FUNCTION Multiplicar
+
+    FUNCTION Sumar(v1, v2)
+        INTEGER :: Sumar;
+        INTEGER, INTENT(IN) v1;
+        INTEGER, INTENT(IN) v2;
+    END FUNCTION Sumar
+
+END INTERFACE
+
+
+c = a + b * 2;
+
+z = Multiplicar(c, x);
+
+resultado = Sumar(a, b);
+
+IF ((a < b) .AND. (.NOT. (c == 0))) THEN
+
+    DO WHILE ((a < 10) .AND. (b < 20))
+
+        SELECT CASE (a)
+
+            CASE (1)
+                c = c + 1;
+
+            CASE (2,3)
+                c = c + 2;
+
+            CASE (4)
+                c = c + 4;
+
+            CASE DEFAULT
+                c = -1;
+
+        END SELECT
+
+        a = a + 1;
+
     ENDDO
-    a = a + 1;
-ENDDO
 
-DO i=0,2,1
-    a = a + 1;
-ENDDO
+ELSE
 
-END PROGRAM pruebasDO
+    c = 999;
+
+ENDIF
+
+END PROGRAM test_final
+
+
+FUNCTION Multiplicar(valor1, valor2)
+
+    REAL :: Multiplicar;
+
+    INTEGER, INTENT(IN) valor1;
+    REAL, INTENT(IN) valor2;
+
+    REAL :: temp;
+
+    temp = (valor1 * valor2) + 3.0;
+
+    Multiplicar = temp;
+
+END FUNCTION Multiplicar
+
+
+FUNCTION Sumar(v1, v2)
+
+    INTEGER :: Sumar;
+
+    INTEGER, INTENT(IN) v1;
+    INTEGER, INTENT(IN) v2;
+
+    INTEGER :: aux;
+
+    aux = v1 + v2;
+
+    Sumar = aux;
+
+END FUNCTION Sumar
